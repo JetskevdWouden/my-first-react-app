a5455dd7b0eb5e102fa3c52a72b1e647c8a16e17
JetskevdWouden <zegmaarjet@gmail.com>
2019-06-11 12:21:03 +0200

    componentas

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  //we define a methos render
   render(){
     return (
+      //we say what it should return
+      //REACT calls the App.rednder() for us
+
+      //render() returns JSX
+      //JSX is a js way of writing HTML
+      //JSX <h1>Hallo</h1>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Welcome to Codaisseur!
           </p>
+          <Greeting />        
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -24,6 +32,17 @@ class App extends React.Component {
     );
   }
 }
+
+class Greeting extends React.Component {
+  render() {
+    return (
+      <h1>Hallo</h1>
+    )
+  }
+}
+
+//call greeting component <Gretting /> ^
+
 
 export default App;
 
